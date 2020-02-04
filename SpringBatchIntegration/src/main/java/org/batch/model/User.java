3c1f7a74cd3732a13d0fdecf32b88f1dfddb6984
nishant <nishant.malviya@gmail.com>
2020-02-04 00:26:45 -0800

    SpringBatch with various lines

---
+++ SpringBatchIntegration/src/main/java/org/batch/model/User.java
@@ -0,0 +1,46 @@
+package org.batch.model;
+
+public class User {
+	private int id;
+	private String name;
+	private String dept;
+	private int salary;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", dept=" + dept + ", salary=" + salary + "]";
+	}
+
+}
