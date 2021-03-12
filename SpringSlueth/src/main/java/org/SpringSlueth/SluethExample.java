e9c77fd981071148f0ca410f08fa75748fbd7fa3
nishant <nishant.malviya@gmail.com>
2021-03-11 20:33:17 -0800

    slueth

---
+++ SpringSlueth/src/main/java/org/SpringSlueth/SluethExample.java
@@ -0,0 +1,12 @@
+package org.SpringSlueth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SluethExample {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SluethExample.class, args);
+	}
+}
---
+++ SpringSlueth/src/main/java/org/SpringSlueth/service/IntegrationService.java
@@ -0,0 +1,8 @@
+package org.SpringSlueth.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IntegrationService {
+
+}
